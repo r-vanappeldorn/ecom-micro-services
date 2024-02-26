b45d7601eb6c5736c4aef4c50b38bbff91e40926
Rajeev van Appeldorn <rvanappeldorn@gmail.com>
2024-02-26 22:13:04 +0100

    feat(auth): implement custom error

--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -1,9 +1,22 @@
 import { ValidationError } from "express-validator";
 
-export class RequestValidationError extends Error {
+import { CustomError } from "./custom-error";
+
+export class RequestValidationError extends CustomError {
     constructor(public errors: ValidationError[]) {
-        super();
+        super("Invalid request parameters");
 
         Object.setPrototypeOf(this, RequestValidationError.prototype);
+    }
+
+    statusCode = 400;
+
+    serializeErrors() {
+        return this.errors.map((error) => {
+            if (error.type === "field") {
+                return { message: error.msg, field: error.path };
+            }
+            return { message: error.msg };
+        });
     }
 }
