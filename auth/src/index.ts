2842ce2de53afed57c1d3ce442b82d8051ac1218
Rajeev van Appeldorn <rvanappeldorn@gmail.com>
2024-02-27 00:11:01 +0100

    feat(auth): set trust proxy to true

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
 import mongoose from "mongoose";
+import cookieSession from "cookie-session";
 
 import {
     currentUserRouter,
@@ -12,7 +13,9 @@ import {
 import { errorHandler } from "./middlewares/error-handler";
 
 const app = express();
+app.set("trust proxy", true);
 app.use(json());
+app.use(cookieSession({ signed: false, secure: true }));
 
 app.use(currentUserRouter);
 app.use(signinRouter);
