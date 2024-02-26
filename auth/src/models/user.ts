98ab38345c0af9b838cf2dfc84b86691996412e2
Rajeev van Appeldorn <rvanappeldorn@gmail.com>
2024-02-26 22:57:18 +0100

    feat(auth): add user model

---
+++ auth/src/models/user.ts
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+interface UserAttrs {
+    email: string;
+    password: string;
+}
+
+interface UserModel extends mongoose.Model<UserDocument> {
+    build(attrs: UserAttrs): UserDocument;
+}
+
+interface UserDocument extends mongoose.Document {
+    email: string;
+    password: string;
+}
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+});
+
+userSchema.statics.build = (attrs: UserAttrs) => new User(attrs);
+
+const User = mongoose.model<UserDocument, UserModel>("User", userSchema);
+
+export { User };
