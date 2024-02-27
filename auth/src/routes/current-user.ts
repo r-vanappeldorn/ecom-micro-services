980c73122b0f0137d08420156ef42dc5c9785e3b
Rajeev van Appeldorn <rvanappeldorn@gmail.com>
2024-02-27 01:42:33 +0100

    feat(auth): add currentuser route

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,24 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+import { InternalServerError } from "../errors/internal-server-error";
 
 const router = express.Router();
 
 router.get("api/users/currentuser", (req, res) => {
-    res.send("Hi");
+    if (!process.env.JWT_KEY) {
+        throw new InternalServerError("JWT_KEY env var is undefined");
+    }
+
+    if (!req.session?.jwt) {
+        return res.send({ currentUser: null });
+    }
+
+    try {
+        const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY);
+        res.status(200).send({ currentUser: payload });
+    } catch (err) {
+        res.send({ currentUser: null });
+    }
 });
 
 export { router as currentUserRouter };
