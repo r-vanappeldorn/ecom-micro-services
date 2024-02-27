03dd67e2c7ec41bec7f7a4f8f316301afac6754c
Rajeev van Appeldorn <rvanappeldorn@gmail.com>
2024-02-27 23:23:00 +0100

    test(auth): add signin helper

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,8 +1,14 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
+import request from "supertest";
+
 import { app } from "../app";
 
 let mongo: MongoMemoryServer;
+
+declare global {
+    var signin: () => Promise<string[]>;
+}
 
 beforeAll(async () => {
     process.env.JWT_KEY = "test";
@@ -26,3 +32,18 @@ afterAll(async () => {
     }
     await mongoose.connection.close();
 });
+
+global.signin = async () => {
+    const email = "test@test.com";
+    const password = "password";
+
+    const res = await request(app)
+        .post("/api/users/signup")
+        .send({
+            email,
+            password,
+        })
+        .expect(201);
+
+    return res.get("Set-Cookie");
+};
