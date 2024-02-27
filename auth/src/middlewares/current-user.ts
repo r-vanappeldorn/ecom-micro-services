ef205a6397616b83be79195be1d5d890e3c5cf13
Rajeev van Appeldorn <rvanappeldorn@gmail.com>
2024-02-27 17:21:27 +0100

    feat(auth): add currentUser middleware

---
+++ auth/src/middlewares/current-user.ts
@@ -0,0 +1,39 @@
+import { Request, Response, NextFunction } from "express";
+import { InternalServerError } from "../errors/internal-server-error";
+import jwt from "jsonwebtoken";
+
+interface UserPayload {
+    id: string;
+    email: string;
+}
+
+declare global {
+    namespace Express {
+        interface Request {
+            currentUser?: UserPayload;
+        }
+    }
+}
+
+export const currentUser = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    if (!process.env.JWT_KEY) {
+        throw new InternalServerError("JWT_KEY env var is undefined");
+    }
+
+    if (!req.session?.jwt) {
+        return res.send({ currentUser: null });
+    }
+
+    try {
+        const payload = jwt.verify(
+            req.session.jwt,
+            process.env.JWT_KEY
+        ) as UserPayload;
+        req.currentUser = payload;
+    } catch (err) {}
+    next();
+};
