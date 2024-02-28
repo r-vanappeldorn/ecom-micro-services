3a727e3ed55524eadcc2b20218f20eb90c707193
Rajeev van Appeldorn <rvanappeldorn@gmail.com>
2024-02-28 17:35:38 +0100

    feat(client): add getCurrentUser helper

---
+++ client/src/helpers/get-current-user.ts
@@ -0,0 +1,28 @@
+import type { Cookies } from '@sveltejs/kit';
+
+type User = {
+	id: string;
+	email: string;
+};
+
+export const getCurrentUser = async (cookies: Cookies): Promise<User | null> => {
+	try {
+		const session = cookies.get('session');
+		if (!session) return null;
+
+		const res = await fetch('http://auth-srv:3000/api/users/currentuser', {
+			method: 'get',
+			headers: {
+				'Content-Type': 'application/json',
+				cookie: `session=${session};`
+			}
+		});
+
+		const data = await res.json();
+
+		return data.currentUser;
+	} catch (err) {
+		console.log(err);
+		return null;
+	}
+};
